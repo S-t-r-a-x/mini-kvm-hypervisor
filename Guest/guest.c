573d2542b0805e50c9277456040dcc92f259c930
Strax <martinovicstrahinja@gmail.com>
2025-10-31 22:19:56 +0100

    Hypervisor and guest template copied over to version A

--- Guest/guest.c
+++ Guest/guest.c
@@ -1,3 +1,8 @@
+#include <stdint.h>
+
+static void outb(uint16_t port, uint8_t value) {
+		asm("outb %0,%1" : /* empty */ : "a" (value), "Nd" (port) : "memory");
+}
 
 void
 __attribute__((noreturn))
@@ -7,7 +12,12 @@ _start(void) {
 	/*
 		INSERT CODE BELOW THIS LINE
 	*/
+	
+	const char *p;
+	uint16_t port = 0xE9;
 
+	for (p = "Hello, world!\n"; *p; ++p)
+		outb(0xE9, *p);
 	/*
 		INSERT CODE ABOVE THIS LINE
 	*/
